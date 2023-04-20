bcdba89ad44b217128791950228c90f032b519af
Rusla nChekina <ruslan.chekina70@gmail.com>
2023-04-20 10:43:29 +0300

    Bootstrap project: deleted exception signature; added auto-increment(GeneratedValue) on entity level in Student Class; used postConstruct annotation;
    all testes are passed;

--- src/main/java/com/foxminded/university/components/DataGenerator.java
+++ src/main/java/com/foxminded/university/components/DataGenerator.java
@@ -1,28 +1,27 @@
 package com.foxminded.university.components;
 
-import com.foxminded.university.exceptions.ExceptionConstants;
 import com.foxminded.university.models.Student;
+import com.foxminded.university.repositories.StudentRepository;
 import com.foxminded.university.utils.CoursesArrayBuilder;
 import com.foxminded.university.utils.GroupNameGenerator;
 import com.foxminded.university.utils.NameGenerator;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class DataGenerator {
     private static String INSERTING_INTO_STUDENTS = "INSERT INTO students(student_id, first_name, last_name) values (?,?,?)";
-    private static String INSERTING_INTO_STUDENTS_GROUPS = "INSERT INTO university_groups(group_id, group_name) " + "VALUES(?,?)";
+    private static String INSERTING_INTO_STUDENTS_GROUPS = "INSERT INTO university_groups(group_id, group_name, student_number) " + "VALUES(?,?,?)";
     private static String INSERTING_INTO_COURSES = "INSERT INTO courses(course_id, course_name, course_description) " +
             "VALUES(?,?,?)";
     private static String INSERTING_INTO_STUDENT_ASSIGNMENTATION = "INSERT INTO course_assignment(assignment_id ,student_id, course_id, group_id )" +
             "VALUES(?,?,?,?)";
-    private int studentLimit = 200;
+    private final int studentLimit = 200;
     private NameGenerator studentNames = new NameGenerator(studentLimit);
     private GroupNameGenerator groupNames = new GroupNameGenerator();
     private final String[] STUDENTS = studentNames.getNames();
@@ -30,59 +29,44 @@ public class DataGenerator {
     private final String[] COURSES = new CoursesArrayBuilder().getCourses();
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private StudentRepository studentRepository;
 
+    @PostConstruct
     public void generate() {
-        try {
-            insertAssignmentationCourses();
-            insertingDataIntoStudents();
-            insertIntoStudentsGroups();
-            insertIntoCourses();
-        } catch (SQLException e) {
-            throw new IllegalArgumentException(ExceptionConstants.SQL_INSERT_ERROR);
-        }
+        insertAssignmentationCourses();
+        insertingDataIntoStudents();
+        insertIntoStudentsGroups();
+        insertIntoCourses();
     }
 
-    private void insertingDataIntoStudents() throws SQLException {
+    public void insertingDataIntoStudents() {
         List<Student> students = new ArrayList<>();
 
-        for (int i = 0, studentID = 1; i < STUDENTS.length; i++) {
+        for (int i = 0; i < STUDENTS.length; i++) {
             Student tempStudent = new Student();
             int randomStudentIndex = (int) (Math.random() * STUDENTS.length);
             String studentName = STUDENTS[randomStudentIndex].split("/")[0];
             String studentLastname = STUDENTS[randomStudentIndex].split("/")[1];
-            tempStudent.setID(studentID);
+
             tempStudent.setFirstName(studentName);
             tempStudent.setLastName(studentLastname);
             students.add(tempStudent);
-            studentID++;
         }
-
-        jdbcTemplate.batchUpdate(INSERTING_INTO_STUDENTS, new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-                Student student = students.get(i);
-                ps.setInt(1, student.getID());
-                ps.setString(2, student.getFirstName());
-                ps.setString(3, student.getLastName());
-            }
-
-            @Override
-            public int getBatchSize() {
-                return students.size();
-            }
-        });
+        studentRepository.saveAll(students);
     }
 
-    private void insertIntoStudentsGroups() throws SQLException {
+
+    private void insertIntoStudentsGroups() {
         int index = 0;
         for (String groupName : GROUPS) {
-            jdbcTemplate.update(INSERTING_INTO_STUDENTS_GROUPS, index, groupName);
+            jdbcTemplate.update(INSERTING_INTO_STUDENTS_GROUPS, index, groupName, 0);
             index++;
         }
 
     }
 
-    private void insertIntoCourses() throws SQLException {
+    private void insertIntoCourses() {
         int index = 0;
         String someDescription = "some course description";
         for (String courseName : COURSES) {
@@ -92,7 +76,7 @@ public class DataGenerator {
 
     }
 
-    private void insertAssignmentationCourses() throws SQLException {
+    private void insertAssignmentationCourses() {
 
         int coursePigmentationLimit = 600;
         int courseRandomId;
